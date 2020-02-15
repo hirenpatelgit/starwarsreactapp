e301685b39cf2ef5fd8776db0adc600a6628fa4e
Hiren Patel <hiren@mycom.world>
2020-02-15 20:56:16 +0530

    Updates

---
+++ precache-manifest.c1b587ca6dd84dcad5e28866049dc16e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2b9734bd3e0918d3063c559f7363b6",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "4c2a160307fc9c0e0fe7",
+    "url": "/starwars/static/css/2.d2a29e6b.chunk.css"
+  },
+  {
+    "revision": "290ee80e14803d825f6f",
+    "url": "/starwars/static/css/main.de59f776.chunk.css"
+  },
+  {
+    "revision": "4c2a160307fc9c0e0fe7",
+    "url": "/starwars/static/js/2.f610767d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/starwars/static/js/2.f610767d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290ee80e14803d825f6f",
+    "url": "/starwars/static/js/main.862ffca0.chunk.js"
+  },
+  {
+    "revision": "cc069465880069b30c84",
+    "url": "/starwars/static/js/runtime-main.3838c57f.js"
+  }
+]);
